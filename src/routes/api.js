fd73e340ddfc2288505507efa586ca901a584b4d
Majaharulsoft <majaharulr1@gmail.com>
2024-09-20 22:31:32 +0600

    create api

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,18 @@
 const express=require('express');
 import { register, login } from '../controllers/authController';
+import { createClass, bookClass } from '../controllers/classController';
+const authMiddleware= require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 
-export default router;
+const InvoiceController = require("../controllers/InvoiceController");
+const FeaturesController = require("../controllers/FeaturesController");
+
+router.post('/create', authMiddleware, createClass);
+router.post('/book', authMiddleware, bookClass);
+
+module.exports=router;
+
